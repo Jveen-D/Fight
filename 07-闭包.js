b97357378d268c958ff46315d9fff768cd785947
duwenjie <d85066965@icloud.com>
2022-02-25 17:58:08 +0800

    闭包有两种用法，一种是共享私有状态的闭包，一种是不共享私有变量的闭包

---
+++ 07-闭包.js
@@ -0,0 +1,40 @@
+// 共享私有变量
+console.log("共享私有变量");
+let uniqueInterger = (function () {
+  // 定义并调用
+  let counter = 0; // 下面函数的私有状态
+  return function () {
+    return counter++;
+  };
+})();
+
+console.log(uniqueInterger()); //0
+console.log(uniqueInterger()); //1
+let a = uniqueInterger();
+let b = uniqueInterger();
+console.log(a); //2
+console.log(b); //3
+console.log("----------------------------------------");
+console.log("不同的私有变量");
+
+// 不同的私有变量
+function counter() {
+  let n = 0;
+  return {
+    count: function () {
+      return n++;
+    },
+    reset: function () {
+      n = 0;
+    },
+  };
+}
+let c = counter(),
+  d = counter();
+console.log(c.count()); //0
+console.log(c.count()); //1
+console.log(d.count()); //0 它们分别计数
+console.log(d.count()); //1 它们分别计数
+c.reset(); //重置c
+console.log(c.count()); //0 重置了c
+console.log(d.count()); //2 但是没有重置d
